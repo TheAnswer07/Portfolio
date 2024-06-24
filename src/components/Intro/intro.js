d6ee90a616c1fbb0d2bfb7da5a58d7ccd51c2b06
TheAnswer07 <moisedia7@gmail.com>
2024-06-24 15:36:08 -0700

    Intro content modified and responsiveness adjusted

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -11,15 +11,7 @@ const Intro = () => {
 <div className="introContent">
 <span className="hello">Hello,</span>
 <span className="introText">I'm <span className="introName">Moussa</span> <br/>Website Designer</span>
-<p className="introPara">I'm a well rounded and results oriented <br/>International Trade, 
-Web Development & <br/>UI/UX Design Professional, <br/>with a track record 
-of success <br/>delivering projects and improving <br/>operational performances. 
-<br/>
-<br/>I quickly adapt to any type of <br/>business environment, with a <br/>superior sense of
-organization <br/>and a great ability to manage <br/>cross-functional teams. 
-<br/>
-<br/>I speak and write fluently in French, <br/>English and Spanish.
-<br/> 
+<p className="introPara">
 <br/>I'm constantly looking for excellence <br/>in the execution of operations 
 <br/>and the achievement of <br/>pre-established objectives.</p>
 <Link> <button className="btn"><img src={btnImg} alt="hireMeImg" className="btnImg"/>Hire Me</button></Link>
@@ -33,3 +25,14 @@ organization <br/>and a great ability to manage <br/>cross-functional teams.
 };
 
 export default Intro;
+
+
+// I'm a well rounded and results oriented <br/>International Trade, 
+// Web Development & <br/>UI/UX Design Professional, <br/>with a track record 
+// of success <br/>delivering projects and improving <br/>operational performances. 
+// <br/>
+// <br/>I quickly adapt to any type of <br/>business environment, with a <br/>superior sense of
+// organization <br/>and a great ability to manage <br/>cross-functional teams. 
+// <br/>
+// <br/>I speak and write fluently in French, <br/>English and Spanish.
+// <br/> 
